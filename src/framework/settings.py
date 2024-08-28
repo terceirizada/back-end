4d148ee705e09f14ff912a7362b174c3f6004424
guilherme <vinespedro55@gmail.com>
2024-08-28 09:09:11 -0300

    fix: deploy vercel

--- src/framework/settings.py
+++ src/framework/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-#&")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["back-end-nu-blond.vercel.app"]
 
 
 AUTH_USER_MODEL = "user.User"
