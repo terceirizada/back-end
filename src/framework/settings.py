ce3c496bfecd8593a99905f49c073475a4db2893
guilherme <vinespedro55@gmail.com>
2024-08-28 09:03:33 -0300

    fix: deploy vercel

--- src/framework/settings.py
+++ src/framework/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "framework/static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
